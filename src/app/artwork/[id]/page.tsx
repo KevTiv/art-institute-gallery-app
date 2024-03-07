c1504106fe7efbeabc269ceca2d9f64a61de6f00
Kevin Tivert <tivertc.kevin@outlook.com>
2024-03-07 15:44:03 +0100

    feat: update layout of page

--- src/app/artwork/[id]/page.tsx
+++ src/app/artwork/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { api } from "@/trpc/server";
 import { HeroImageViewer } from "@/components/ImageViewer";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { Heart } from "lucide-react";
+import { useArtworkStore } from "@/store/favoriteArtwork";
+import { FavoritesActions } from "@/components/FavoritesActions";
 
 type ArtworkPageParams = {
   params: { id: string };
@@ -15,18 +19,25 @@ export default async function Artwork({ params }: ArtworkPageParams) {
 
   return (
     <main className="flex min-h-screen flex-col gap-4 bg-gradient-to-b from-slate-100 to-slate-200 px-12 py-4 text-slate-900">
-      <HeroImageViewer artwork={artwork} imageId={artwork?.image_id} />
+      <div className={"relative"}>
+        <HeroImageViewer artwork={artwork} imageId={artwork?.image_id} />
+        <FavoritesActions artwork={artwork} />
+      </div>
 
-      <h3>
+      <h3 className={"text-2xl font-semibold"}>
         {artwork.title} by {artist.title}
       </h3>
-      {artwork.description}
 
-      <h3>{artist.title}</h3>
+      <p className={"text-sm"}>
+        {artwork.description?.replace(/<\/?p>|&quot;|<em>|<\/em>/g, "")}
+      </p>
 
-      <div>{artist.birth_date}</div>
-      <div>{artist.death_date}</div>
-      <div>{artist.description}</div>
+      {artwork.date_start && artwork.date_end && artist.title && (
+        <p className={"text-xs"}>
+          {artist.title} started working on this piece in {artwork.date_start}{" "}
+          and finsihed in {artwork.date_end}
+        </p>
+      )}
     </main>
   );
 }
