4b8e42c19c040191daa693a62e79105056b0ea6b
Kevin Tivert <tivertc.kevin@outlook.com>
2024-03-07 17:22:06 +0100

    fix: wrap pages in suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { unstable_noStore as noStore } from "next/cache";
 import { api } from "@/trpc/server";
 import { ArtworkGalleryViewer } from "@/components/ArtworkGalleryViewer";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
 export default async function Home() {
   noStore();
@@ -9,8 +11,10 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col gap-4  px-12 py-4 text-slate-900">
-      <h3 className={"mx-2 text-2xl font-semibold"}>Gallery Collection</h3>
-      <ArtworkGalleryViewer allArtworksResponse={allArtworksResponse} />
+      <Suspense fallback={<Skeleton className="h-full w-full" />}>
+        <h3 className={"mx-2 text-2xl font-semibold"}>Gallery Collection</h3>
+        <ArtworkGalleryViewer allArtworksResponse={allArtworksResponse} />
+      </Suspense>
     </main>
   );
 }
