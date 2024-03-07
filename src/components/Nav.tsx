8581a7bc1743ff8cc35361017b39a9ba5fe3ae97
Kevin Tivert <tivertc.kevin@outlook.com>
2024-03-07 15:42:41 +0100

    feat: creat nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,13 @@
 import { SearchArtwork } from "@/components/SearchArtwork";
 import Link from "next/link";
+import { Palette } from "lucide-react";
 
 export function Nav() {
   return (
     <nav className="bg-secondary flex justify-between px-4 py-2">
-      <h1 className="text-center text-xl">Welcome to mock Gallery</h1>
+      <Link className="flex items-center gap-2 text-center text-xl" href={"/"}>
+        <Palette /> Gallery
+      </Link>
       <div className="flex items-center gap-12">
         <SearchArtwork />
         <Link className={"text-xl"} href={"/favorites"}>
