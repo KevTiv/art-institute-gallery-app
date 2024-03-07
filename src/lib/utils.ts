656b283924470d7051173febba4861f6ff7a68b0
Kevin Tivert <tivertc.kevin@outlook.com>
2024-03-07 12:12:45 +0100

    feat: shadcn tailwind utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function constructArtImageUrl(imageId?: string | null) {
+  if (imageId) {
+    return `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
+  }
 }
